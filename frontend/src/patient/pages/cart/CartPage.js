dbb3a891a926e5dbcb1aacfc4a6359841d8aa1d3
Shampoo <kareemelkaderi@gmail.com>
2023-11-11 12:56:25 +0200

    fix: cart svg white background

--- frontend/src/patient/pages/cart/CartPage.js
+++ frontend/src/patient/pages/cart/CartPage.js
@@ -27,17 +27,20 @@ const CartPage = (props) => {
   return (
     <div className={classes.cartpage}>
       <div className='col-5'>
-        <div  className={classes.backarrow}>
-      <img src={arrow}
-              style={{ width: '20px', height: 'auto' }}
-            />
-        <button className={classes.back} /* onClick={() => Handle order click }*/> Back</button>
+        <div className={classes.backarrow}>
+          <img src={arrow}
+            style={{ width: '20px', height: 'auto' }}
+          />
+          <button className={classes.back} /* onClick={() => Handle order click }*/> Back</button>
         </div>
       </div>
-      <div className='col-5'>
+      <div className='col-5 position-relative'>
+        <svg className={classes.backgroundsvg} xmlns="http://www.w3.org/2000/svg"fill="none">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M547.307 26.9986C368.24 89.514 156.939 120.347 62.9562 280.934C-34.6226 447.664 -8.05373 659.342 79.784 825.846C158.797 975.621 334.11 1032.03 494.246 1098.48C649.537 1162.91 805.342 1230.36 973.602 1196.71C1172.6 1156.91 1410.6 1086.96 1479.12 901.499C1546.49 719.157 1344.65 567.738 1271.02 391.707C1203.62 230.596 1231.4 8.85592 1072.15 -71.3951C910.76 -152.727 722.227 -34.0694 547.307 26.9986Z" fill="white" />
+        </svg>
         <div className={`d-flex flex-row ${classes.rightSection} justify-space-between`}>
-          <div>YOUR SHOPPING CART</div>
-          <div style={{ width: "30px" }}>
+          <div style={{zIndex:1}}>YOUR SHOPPING CART</div>
+          <div style={{ width: "30px",zIndex:1}}>
             <img
               src={icon}
               style={{ width: '30px', height: 'auto' }}
@@ -57,18 +60,18 @@ const CartPage = (props) => {
                 </div>
               </div>
             </div>
-            
+
 
           ) : (
             <div className={classes.empty}>
-            <p >Cart seems empty?
-              <button className={classes.more} >Shop more</button>
+              <p >Cart seems empty?
+                <button className={classes.more} >Shop more</button>
 
-            </p>
+              </p>
             </div>
           )}
-          </div>
-        
+        </div>
+
       </div>
       <div className='col-2'></div>
 
--- frontend/src/patient/pages/cart/MedSummaryItem.js
+++ frontend/src/patient/pages/cart/MedSummaryItem.js
@@ -4,9 +4,9 @@ import CartContext from './Cart';
 
 const MedSummaryItem = (props) => {
     const cartCtx = useContext(CartContext);
-    
+
     const increment = () => {
-        cartCtx.addItem({id: props.id, image:props.image, title: props.title, desc: props.desc, price: props.price, quantity: 1});
+        cartCtx.addItem({ id: props.id, image: props.image, title: props.title, desc: props.desc, price: props.price, quantity: 1 });
     };
 
     const decrement = () => {
@@ -15,35 +15,35 @@ const MedSummaryItem = (props) => {
 
     return (
         <div>
-            
-             <div style={{ display: 'flex', alignItems: 'center' }}>
+
+            <div style={{ display: 'flex', alignItems: 'center' }}>
                 <img
-            src={props.image}
-            alt={props.description}
-            style={{ width: '100px', height: 'auto' }}
-          />
-              
-               <div className={classes.items}> 
-                <h2 className={classes.label}>{props.label}</h2>
-                
-                <span > {props.description}</span>
-                
-                <div>
-                    <label className={classes.price}> $ {props.price}</label>
-                    <br></br>
-                    <span >Quantity: </span>
-                <button className={classes.inc} onClick={decrement}>-</button>
-                <span > {props.quantity} </span>
-                <button className={classes.inc} onClick={increment}>+</button>
-            </div>
-            
-            </div>
-            
+                    src={props.image}
+                    alt={props.description}
+                    style={{ width: '100px', height: 'auto' }}
+                />
+
+                <div className={classes.items}>
+                    <h2 className={classes.label}>{props.label}</h2>
+
+                    <span > {props.description}</span>
+
+                    <div>
+                        <label className={classes.price}> $ {props.price}</label>
+                        <br></br>
+                        <span >Quantity: </span>
+                        <button className={classes.inc} onClick={decrement}>-</button>
+                        <span > {props.quantity} </span>
+                        <button className={classes.inc} onClick={increment}>+</button>
+                    </div>
+
+                </div>
+
             </div>
             <hr></hr>
-            </div>
+        </div>
 
-           
+
     );
 };
 
