522f3a76e03a86b0b6ae532783a98393934e9161
Youssef Mostafa <88552711+Youssef-Hossam-Mostafa@users.noreply.github.com>
2023-11-13 20:57:05 +0200

    fix: cors error NOT TESTED

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,31 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const globalErrorHandler = require("./Controllers/errorController");
+const express = require('express');
+const dotenv = require('dotenv');
+const globalErrorHandler = require('./Controllers/errorController');
 
-const AppError = require("./utils/appError");
-const adminRouter = require("./routes/adminRoutes");
-const patientRouter = require("./routes/patientRoutes");
-const medicineRouter = require("./routes/medicineRoutes");
-const pharmacistRouter = require("./routes/pharmacistRoutes");
-const pharmacyRouter = require("./routes/pharmacyRoutes");
+const AppError = require('./utils/appError');
+const adminRouter = require('./routes/adminRoutes');
+const patientRouter = require('./routes/patientRoutes');
+const medicineRouter = require('./routes/medicineRoutes');
+const pharmacistRouter = require('./routes/pharmacistRoutes');
+const pharmacyRouter = require('./routes/pharmacyRoutes');
 const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //to allow sending cookies if any
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-  next();
-});
+app.use('/pharmacy', pharmacyRouter);
+app.use('/admins', adminRouter);
+app.use('/pharmacist', pharmacistRouter);
+app.use('/patients', patientRouter);
+app.use('/medicine', medicineRouter);
 
-app.use("/pharmacy", pharmacyRouter);
-app.use("/admins", adminRouter);
-app.use("/pharmacist", pharmacistRouter);
-app.use("/patients", patientRouter);
-app.use("/medicine", medicineRouter);
-
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`));
 });
 
