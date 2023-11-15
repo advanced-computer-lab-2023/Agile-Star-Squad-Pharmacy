7d484181645302c19dcd60ed6449546d1d895fdd
Assem Mohamed <assem08mohamed@gmail.com>
2023-11-16 00:46:48 +0200

    fix: added relation to relate address with patient

--- backend/models/addressModel.js
+++ backend/models/addressModel.js
@@ -1,19 +1,25 @@
 const mongoose = require('mongoose');
 
 const addressSchema = new mongoose.Schema({
-  country: {
-    type: String,
-    required: true
-  },
-  city: {
-    type: String,
-    required: true
-  },
-  street: {
-    type: String,
-    required: true
-  }
+    country: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    street: {
+        type: String,
+        required: true
+    },
+  patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Patient',
+    },
+
 });
+
 
 const Address = mongoose.model('Address', addressSchema);
 
