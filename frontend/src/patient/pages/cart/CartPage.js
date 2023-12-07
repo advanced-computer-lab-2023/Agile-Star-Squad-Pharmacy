15746a62346ce60a0d25f71282667467c68b5944
Habibaelkabbany <helkabbany2@gmail.com>
2023-12-08 00:09:32 +0200

    oki

--- frontend/src/patient/pages/cart/CartPage.js
+++ frontend/src/patient/pages/cart/CartPage.js
@@ -6,6 +6,7 @@ import background from './cartbackground.jpg';
 import icon from './cartIcon.png';
 import arrow from './ArrowLeft.png';
 import { Link, useNavigate } from 'react-router-dom';
+import NavBar from '../../../shared/components/NavBar/NavBar';
 
 
 
@@ -32,8 +33,15 @@ const CartPage = (props) => {
   };
 
   return (
+   
+      
+      // <NavBar
+      
+  
     <div className={classes.cartpage}>
+      
       <div className='col-5'>
+      
         <div className={classes.backarrow}>
           <img src={arrow}
             style={{ width: '20px', height: 'auto' }}
@@ -42,11 +50,11 @@ const CartPage = (props) => {
         </div>
       </div>
       <div className='col-5 position-relative'>
-        <svg className={classes.backgroundsvg} xmlns="http://www.w3.org/2000/svg"fill="none">
+        <svg className={classes.backgroundsvg} xmlns="http://www.w3.org/2000/svg"fill="none" style={{zIndex:0}}>
           <path fill-rule="evenodd" clip-rule="evenodd" d="M547.307 26.9986C368.24 89.514 156.939 120.347 62.9562 280.934C-34.6226 447.664 -8.05373 659.342 79.784 825.846C158.797 975.621 334.11 1032.03 494.246 1098.48C649.537 1162.91 805.342 1230.36 973.602 1196.71C1172.6 1156.91 1410.6 1086.96 1479.12 901.499C1546.49 719.157 1344.65 567.738 1271.02 391.707C1203.62 230.596 1231.4 8.85592 1072.15 -71.3951C910.76 -152.727 722.227 -34.0694 547.307 26.9986Z" fill="white" />
         </svg>
         <div className={`d-flex flex-row ${classes.rightSection} justify-space-between`}>
-          <div style={{zIndex:1}}>YOUR SHOPPING CART</div>
+          <div id ='titles' style={{zIndex:1}}>YOUR SHOPPING CART</div>
           <div style={{ width: "30px",zIndex:1}}>
             <img
               src={icon}
@@ -60,10 +68,10 @@ const CartPage = (props) => {
           {medSummaryItems.length > 0 ? (
             <div className={classes.summary}>
               <div className={classes.cartItem}>{medSummaryItems}</div>
-              <div>
+              <div className='hello'>
                 <h2 className={classes.total}>Total Amount: $ {cartCtx.total}</h2>
                 <div className={classes.checkoutButton}>
-                  <Link to="../payment/AddingInfo">
+                  <Link to="../payment/temp" style={{ all: 'unset' }}>
                 <button className={classes.check} >
                   Checkout
                 </button>
@@ -87,6 +95,9 @@ const CartPage = (props) => {
       <div className='col-2'></div>
 
     </div>
+   
+   
+    
   );
 };
 
