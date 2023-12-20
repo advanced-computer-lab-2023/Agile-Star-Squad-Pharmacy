1bede6f6c93979ef32e5de1c4840a765a26539e0
Youssef Mostafa <88552711+Youssef-Hossam-Mostafa@users.noreply.github.com>
2023-12-20 03:06:17 +0200

    feat: finish confirmation modals in pharmacy

--- frontend/src/pharmacist/pages/AddMedicine.js
+++ frontend/src/pharmacist/pages/AddMedicine.js
@@ -1,20 +1,19 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import Dropzone from 'react-dropzone';
 import medicinalUseEnum from '../../shared/util/MedicinalUseEnum';
 import Modal from '../../shared/components/Modal/Modal';
 import styles from '../components/AddMedicine.module.css';
 import axios from 'axios';
-import uploadImg from "../../assets/patientAccount/upload.png"
+import uploadImg from '../../assets/patientAccount/upload.png';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import storage from '../..';
-import Select from 'react-select'
+import Select from 'react-select';
 // import { toastMe } from '../../shared/util/functions';
-import {toast} from "react-toastify";
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { toastMeSuccess } from '../../shared/util/functions';
-
-
+import ConfirmationModal from '../../shared/components/ConfirmationModal/ConfirmationModal';
 
 const AddMedicine = (props) => {
   const [name, setName] = useState();
@@ -24,23 +23,26 @@ const AddMedicine = (props) => {
   const [quantity, setQuantity] = useState(0);
   const [sales, setSales] = useState(0);
   const [files, setFiles] = useState([]);
-  const [imageUrl, setImageUrl] = useState("");
-  const [medicinalUse, setMedicinalUse] = useState("");
+  const [imageUrl, setImageUrl] = useState('');
+  const [medicinalUse, setMedicinalUse] = useState('');
   const isAdd = props.medicine == null;
+
+  const [showArchiveModal, setShowArchiveModal] = useState(false);
+  const [showSaveModal, setShowSaveModal] = useState(false);
 
   useEffect(() => {
     if (props.medicine != null) {
-      console.log(props.medicine.name)
+      console.log(props.medicine.name);
       const medicine = props.medicine;
       setName(medicine.name);
       setDescription(medicine.description);
-      setIngredients(medicine.activeIngredient)
+      setIngredients(medicine.activeIngredient);
       setPrice(medicine.price);
       setQuantity(medicine.quantity);
       setSales(medicine.profit);
-      setMedicinalUse({ label: medicine.medicinalUse })
+      setMedicinalUse({ label: medicine.medicinalUse });
       setImageUrl(medicine.image);
-      setFiles([{ name: medicine.image, preview: medicine.image }])
+      setFiles([{ name: medicine.image, preview: medicine.image }]);
     }
   }, []);
 
@@ -54,7 +56,7 @@ const AddMedicine = (props) => {
 
   const handleDescriptionChange = (event) => {
     setDescription(event.target.value);
-  }
+  };
 
   const handlePriceChange = (event) => {
     setPrice(event.target.value);
@@ -81,7 +83,14 @@ const AddMedicine = (props) => {
 
   const handleSave = async () => {
     // Check if any of the required fields is empty
-    if (!name || !ingredients || !price || !quantity || !sales || files.length == 0) {
+    if (
+      !name ||
+      !ingredients ||
+      !price ||
+      !quantity ||
+      !sales ||
+      files.length == 0
+    ) {
       alert('Please fill in all the fields before saving.');
       return;
     }
@@ -93,16 +102,15 @@ const AddMedicine = (props) => {
       quantity,
       profit: sales,
       medicinalUse: medicinalUse.label,
-
     };
 
-    if (!files[0].name.includes("http")) {
+    if (!files[0].name.includes('http')) {
       let downloadUrl;
       const imageRef = ref(storage, `${files[0].name}`);
       await uploadBytesResumable(imageRef, files[0]).then(async (snapshot) => {
         downloadUrl = await getDownloadURL(snapshot.ref);
       });
-      data["image"] = downloadUrl;
+      data['image'] = downloadUrl;
     }
 
     let toastMsg;
@@ -114,34 +122,51 @@ const AddMedicine = (props) => {
         .catch((err) => {
           console.error(err);
         });
-        toastMsg = "Updated medicine successfully";
+      toastMsg = 'Updated medicine successfully';
     } else {
       const response = await axios
-        .post(
-          'http://localhost:4000/medicine',
-          data,
-          { withCredentials: true }
-        )
+        .post('http://localhost:4000/medicine', data, { withCredentials: true })
         .then(() => {
           alert('Medicine added successfully');
         })
         .catch((err) => {
           console.error(err);
         });
-        toastMsg = "Added medicine successfully";
+      toastMsg = 'Added medicine successfully';
     }
     if (data.image == null) {
       data.image = files[0].name;
     }
     toastMeSuccess(toastMsg);
     props.exit(data);
-
   };
 
   return ReactDOM.createPortal(
     <Modal exit={props.exit}>
+      {showArchiveModal && (
+        <ConfirmationModal
+          exit={() => setShowArchiveModal(false)}
+          confirm={() => {
+            handleArchive();
+            setShowArchiveModal(false);
+          }}
+          text="Archive Medicine"
+        />
+      )}
+      {showSaveModal && (
+        <ConfirmationModal
+          exit={() => setShowSaveModal(false)}
+          confirm={() => {
+            handleSave();
+            setShowSaveModal(false);
+          }}
+          text="Save Medicine"
+        />
+      )}
       <div className={styles.titleContainer}>
-        <h2 className={styles.addMedicineText}>{isAdd ? "Add" : "Edit"} Medicine</h2>
+        <h2 className={styles.addMedicineText}>
+          {isAdd ? 'Add' : 'Edit'} Medicine
+        </h2>
       </div>
       <div>
         <label className={styles.label}>Medicine Name</label>
@@ -207,30 +232,39 @@ const AddMedicine = (props) => {
       </div>
       <label>Medicinal Use</label>
       <Select
-        className='mb-3 mt-1'
+        className="mb-3 mt-1"
         value={medicinalUse}
         styles={customStyles}
         options={medicinalUses}
         onChange={(value) => setMedicinalUse(value)}
-        required />
+        required
+      />
       <label className={styles.label}>Image</label>
       <MyDropzone files={files} setFiles={setFiles} maxFiles={1} />
       <div className={styles.archiveContainer}>
-        <button onClick={handleArchive} className={styles.archiveButton}>
+        <button
+          onClick={() => setShowArchiveModal(true)}
+          className={styles.archiveButton}
+        >
           ARCHIVE
         </button>
       </div>
       <div className={styles.saveButtonContainer2}>
-        <button onClick={handleSave} className={styles.saveButton}>
+        <button
+          onClick={() => {
+            setShowSaveModal(true);
+          }}
+          className={styles.saveButton}
+        >
           SAVE
         </button>
       </div>
-    </Modal>
-    , document.getElementById("backdrop-root"));
+    </Modal>,
+    document.getElementById('backdrop-root')
+  );
 };
 
 export default AddMedicine;
-
 
 const MyDropzone = (props) => {
   const files = props.files;
@@ -244,13 +278,13 @@ const MyDropzone = (props) => {
       console.log(
         Object.assign(acceptedFiles[0], {
           preview: URL.createObjectURL(acceptedFiles[0]),
-        }),
+        })
       );
       setFiles((prevFiles) => [
         ...acceptedFiles.map((file) =>
           Object.assign(file, {
             preview: URL.createObjectURL(file),
-          }),
+          })
         ),
       ]);
     }
@@ -288,7 +322,7 @@ const MyDropzone = (props) => {
                 <aside style={thumbsContainer}>{thumbs}</aside>
               )}
               {files.length == 0 && (
-                <div className='d-flex flex-column align-items-center'>
+                <div className="d-flex flex-column align-items-center">
                   <img height={50} src={uploadImg} />
                   <div className="mt-3">Drag & drop files or Browse</div>
                   <div className={styles.dropzoneSubtitle}>
@@ -305,17 +339,17 @@ const MyDropzone = (props) => {
 };
 
 const medicinalUses = [
-  { label: "Fever" },
-  { label: "Headache" },
-  { label: "Eyes" },
-  { label: "Cough" },
-  { label: "Nutrition" },
-  { label: "Baby Products" },
-  { label: "Pain Relief" },
-  { label: "Stomach" },
-  { label: "Insomnia" },
-  { label: "First Aid" },
-]
+  { label: 'Fever' },
+  { label: 'Headache' },
+  { label: 'Eyes' },
+  { label: 'Cough' },
+  { label: 'Nutrition' },
+  { label: 'Baby Products' },
+  { label: 'Pain Relief' },
+  { label: 'Stomach' },
+  { label: 'Insomnia' },
+  { label: 'First Aid' },
+];
 
 const thumb = {
   display: 'inline-flex',
@@ -350,16 +384,13 @@ const thumbsContainer = {
   marginTop: 8,
 };
 
-
-
-
 const customStyles = {
   control: (provided, state) => ({
     ...provided,
     backgroundColor: '#f5f5f5',
     border: 'none',
     borderBottom: '1px solid #E2E4E5',
-    textAlign: 'start'
+    textAlign: 'start',
   }),
 
   placeholder: (provided, state) => ({
@@ -381,15 +412,15 @@ const customStyles = {
     ...provided,
     borderRadius: '14px',
     fontSize: '14px',
-    fontWeight: state.isFocused ? "500" : "400",
-    color: state.isFocused ? "black" : "#666666",
-    textAlign: "left",
-    backgroundColor: "transparent"
+    fontWeight: state.isFocused ? '500' : '400',
+    color: state.isFocused ? 'black' : '#666666',
+    textAlign: 'left',
+    backgroundColor: 'transparent',
   }),
   value: (provided) => ({
     ...provided,
     borderRadius: '20px',
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
   }),
   singleValue: (provided) => ({
     ...provided,
@@ -397,24 +428,24 @@ const customStyles = {
   }),
   valueContainer: (provided) => ({
     ...provided,
-    backgroundColor: "transparent"
+    backgroundColor: 'transparent',
   }),
   menuList: (base) => ({
     ...base,
 
-    "::-webkit-scrollbar": {
-      width: "3px",
-      height: "0px",
+    '::-webkit-scrollbar': {
+      width: '3px',
+      height: '0px',
     },
-    "::-webkit-scrollbar-track": {
-      background: "transparent"
+    '::-webkit-scrollbar-track': {
+      background: 'transparent',
     },
-    "::-webkit-scrollbar-thumb": {
-      background: "#888",
+    '::-webkit-scrollbar-thumb': {
+      background: '#888',
       borderRadius: '3px',
     },
-    "::-webkit-scrollbar-thumb:hover": {
-      background: "#555"
-    }
-  })
+    '::-webkit-scrollbar-thumb:hover': {
+      background: '#555',
+    },
+  }),
 };
